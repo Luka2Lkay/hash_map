15c2498fb7c670d69d5b34fdd4a71bed46f80f31
Luka <lukhanyo95@gmail.com>
2024-08-28 18:43:58 +0200

    start the set method

--- hash_map.js
+++ hash_map.js
@@ -1,6 +1,8 @@
 class HashMap {
   constructor() {
-    this.bucket = new Array(7);
+    this.buckets = new Array(16).fill();
+    this.size = 0;
+    this.loadFactor = 0.75;
   }
 
   hash(key) {
@@ -8,9 +10,39 @@ class HashMap {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode =
-        primeNumber * key.charCodeAt(i) + (hashCode % this.bucket.length);
+        (primeNumber * key.charCodeAt(i) + hashCode) % this.buckets.length;
     }
 
     return hashCode;
   }
+
+  set(key, value) {
+    const index = this.hash(key)
+    let bucket = this.buckets[index]
+   
+
+    if(index < 0 || index >= this.buckets.length) throw new Error("The index is out of bound")
+
+    if(!bucket) {
+        bucket = []
+    } 
+
+
+    for(let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] == key){
+            bucket[i][1] == value;
+            return
+        } 
+    }
+
+    bucket.push([key, value])
+    this.size++
+
+    if(this.size / this.buckets.length >= this.loadFactor) 
+    return this.buckets
+  }
 }
+
+const newHash = new HashMap()
+
+console.log(newHash.set('luka', 'lukas'))
