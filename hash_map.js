779d1f00d66dcce8602e717b97bf96d497df1fb2
Luka <lukhanyo95@gmail.com>
2024-08-28 17:49:22 +0200

    create the hash(key) method

--- hash_map.js
+++ hash_map.js
@@ -1,5 +1,16 @@
 class HashMap {
-    constructor() {
-        
+  constructor() {
+    this.bucket = new Array(7);
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode =
+        primeNumber * key.charCodeAt(i) + (hashCode % this.bucket.length);
     }
+
+    return hashCode;
+  }
 }
