d56316362bbb5abd894b14aaa96ae7ed3699b5ab
Luka <lukhanyo95@gmail.com>
2024-08-29 18:52:21 +0200

    update the set

--- hash_map.js
+++ hash_map.js
@@ -1,6 +1,6 @@
 class HashMap {
   constructor() {
-    this.buckets = new Array(16).fill();
+    this.buckets = new Array(16);
     this.size = 0;
     this.loadFactor = 0.75;
   }
@@ -17,32 +17,44 @@ class HashMap {
   }
 
   set(key, value) {
-    const index = this.hash(key)
-    let bucket = this.buckets[index]
-   
+    const index = this.hash(key);
+    let bucket = this.buckets[index];
 
-    if(index < 0 || index >= this.buckets.length) throw new Error("The index is out of bound")
+    if (index < 0 || index >= this.buckets.length)
+      throw new Error("The index is out of bound");
 
-    if(!bucket) {
-        bucket = []
-    } 
-
-
-    for(let i = 0; i < bucket.length; i++) {
-        if (bucket[i][0] == key){
-            bucket[i][1] == value;
-            return
-        } 
+    if (!bucket) {
+      bucket = [];
     }
 
-    bucket.push([key, value])
-    this.size++
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] == key) {
+        bucket[i][1] == value;
+        return;
+      }
+    }
 
-    if(this.size / this.buckets.length >= this.loadFactor) 
-    return this.buckets
+    bucket.push([key, value]);
+    this.size++;
+
+    if (this.size / this.buckets.length >= this.loadFactor) {
+      const initialBuckets = this.buckets();
+      this.buckets = new Array(this.buckets.length * 3);
+
+      for (let i = 0; i < initialBuckets.length; i++) {
+        for (let j = 0; j < initialBuckets[i]; i++) {
+          const [key, value] = initialBuckets[i][j];
+          this.set(key, value);
+        }
+      }
+    }
   }
 }
 
-const newHash = new HashMap()
+const newHash = new HashMap();
 
-console.log(newHash.set('luka', 'lukas'))
+console.log(newHash.set("luka", "lukas"));
+console.log(newHash.set("apple", "red"));
+console.log(newHash.set("banana", "yellow"));
+console.log(newHash.set("carrot", "orange"));
+console.log(newHash.set("dog", "brown"));
