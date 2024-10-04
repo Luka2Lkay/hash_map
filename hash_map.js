0fd0080e1eea1ee2c6d1604f603281c8eb35694e
Luka <lukhanyo95@gmail.com>
2024-10-04 18:28:39 +0200

    use unshift

--- hash_map.js
+++ hash_map.js
@@ -1,5 +1,5 @@
 const resize = (bucket, hashKey) => {
-  let newBuckets = new Array(bucket.length * 2);
+  let newBuckets = new Array(bucket.length * 27);
 
   for (let i = 0; i < bucket.length; i++) {
     if (bucket) {
@@ -15,7 +15,6 @@ class HashMap {
   constructor() {
     this.buckets = new Array(16);
     this.numberOfEntries = 0;
-    this.loadFactor = this.numberOfEntries / this.buckets.length;
   }
 
   hash(key) {
@@ -32,19 +31,43 @@ class HashMap {
   set = (key, value) => {
     this.numberOfEntries++;
     const index = this.hash(key);
+    const loadFactor = this.numberOfEntries / this.buckets.length;
+    console.log(loadFactor)
 
+    // if(this.loadFactor > 0.75) {
+    //   resize(this.buckets, index);
+    // }
+
+    console.log(this.numberOfEntries/this.buckets.length)
     if (index < 0 || index >= this.buckets.length)
       throw new Error("The index is out of bound");
 
     if (this.buckets[index]) {
-      for (let i = 0; i < this.buckets[index].length; i++) {
-        if (this.buckets[index][0][0] === key) {
-          this.buckets[index][0][1] = value;
-        }
-      }
+      
+      // this.buckets.forEach(item => {
+      //   console.log(item)
+      // })
+      // for (let i = 0; i < this.buckets.length; i++) {
+
+      //   console.log(this.buckets)
+      //   // if (this.buckets[index][0][0] === key) {
+      //   //   this.buckets[index][0][1] = value;
+      //   // }
+      // }
+      this.buckets[index].unshift([key,value])
+
+  //  this.buckets[index].forEach(([key,value]) => {
+    
+  //  })
+      
+      // console.log(this.buckets[index])
     } else {
+
       this.buckets[index] = [[key, value]];
+      console.log(this.buckets[index])
     }
+
+
   };
 
   get = (key) => {
@@ -52,7 +75,7 @@ class HashMap {
 
     if (!this.buckets[index]) return null;
 
-    return this.buckets[index][0][1];
+    return this.buckets[index].find(x => x[0] === key);
   };
 }
 
@@ -62,8 +85,27 @@ newHash.set("banana", "yellow");
 newHash.set("banana", "pink");
 newHash.set("carrot", "orange");
 newHash.set("dog", "brown");
+newHash.set('elephant', 'gray')
+newHash.set('frog', 'green')
+newHash.set('grape', 'purple')
+newHash.set('hat', 'black')
+newHash.set('ice cream', 'white')
+newHash.set('jacket', 'blue')
+newHash.set('kite', 'pink')
+newHash.set('lion', 'golden')
+
+
 console.log(newHash.get("apple"));
+console.log(newHash.get("banana"));
 console.log(newHash.get("banana"));
 console.log(newHash.get("carrot"));
 console.log(newHash.get("dog"));
-console.log(newHash.hash("apple"));
+console.log(newHash.get('elephant'))
+// console.log(newHash.get('frog'))
+// console.log(newHash.get('grape'))
+// console.log(newHash.get('hat'))
+// console.log(newHash.get('ice cream'))
+// console.log(newHash.get('jacket'))
+// console.log(newHash.get('kite'))
+// console.log(newHash.get('lion'))
+
