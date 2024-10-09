90ca6119b9635fe2a4fa9f8c51e182b6952a1b74
Luka <lukhanyo95@gmail.com>
2024-10-09 18:45:52 +0200

    remove comments

--- hash_map.js
+++ hash_map.js
@@ -1,17 +1,17 @@
-const defaultConfig = {
-  hashMapSize: 16,
-  loadFactor: 0.75,
-};
 class HashMap {
-  #_buckets;
-  constructor(config) {
-    Object.entries({ ...defaultConfig, ...config }).forEach(([key, value]) => {
-      this[key] = value;
-    });
+  #buckets;
+  // #hashMapSize;
+  #numberOfEntries;
 
-    // this.bucketsSize = 16;
-    this.#_buckets = new Array(this.hashMapSize);
-    this.numberOfEntries = 0;
+  constructor() {
+    Object.entries({ ...{ hashMapSize: 16, loadFactor: 0.75 } }).forEach(
+      ([key, value]) => {
+        this[key] = value;
+      }
+    );
+
+    this.#buckets = new Array(this.hashMapSize);
+    this.#numberOfEntries = 0;
   }
 
   hash(key) {
@@ -24,21 +24,21 @@ class HashMap {
     }
     return hashCode;
   }
-  #findAvailableCodeForKey(key) {
+
+  #findUnusedCodeForKey(key) {
     let hashCode = this.hash(key);
     if (
-      this.#_buckets[hashCode] === undefined ||
-      this.#_buckets[hashCode].key === key
+      !this.#buckets[hashCode] ||
+      this.#buckets[hashCode].key === key
     ) {
       return hashCode;
     } else {
-      // Fixed collision
       let jumpStep = 1;
-      while (jumpStep <= this.#_buckets.length) {
-        const newHashCode = (hashCode + jumpStep) % this.#_buckets.length;
+      while (jumpStep <= this.#buckets.length) {
+        const newHashCode = (hashCode + jumpStep) % this.#buckets.length;
         if (
-          this.#_buckets[newHashCode] === undefined ||
-          this.#_buckets[newHashCode].key === key
+          this.#buckets[newHashCode] === undefined ||
+          this.#buckets[newHashCode].key === key
         ) {
           return newHashCode;
         }
@@ -50,107 +50,47 @@ class HashMap {
 
   currentCapacity() {
     return (
-      this.#_buckets.filter((obj) => obj !== undefined).length /
-      this.#_buckets.length
+      this.#buckets.filter((obj) => obj !== undefined).length /
+      this.#buckets.length
     );
   }
 
   #expandBuckets() {
-    const oldArr = this.#_buckets.slice();
+    const oldArr = this.#buckets.slice();
     const newSize = oldArr.length * 2;
     const newArr = new Array(newSize);
-    this.#_buckets = newArr;
+    this.#buckets = newArr;
     oldArr.forEach((item) => {
       if (item !== undefined) {
-        const hashCode = this.#findAvailableCodeForKey(item.key);
-        this.#_buckets[hashCode] = { key: item.key, value: item.value };
+        const hashCode = this.#findUnusedCodeForKey(item.key);
+        this.#buckets[hashCode] = { key: item.key, value: item.value };
       }
     });
   }
 
   set(key, value) {
-    const hashCode = this.#findAvailableCodeForKey(key);
-    this.#_buckets[hashCode] = { key, value };
-    this.numberOfEntries++;
+    const hashCode = this.#findUnusedCodeForKey(key);
+    this.#buckets[hashCode] = { key, value };
+    this.#numberOfEntries++;
     console.log(this.currentCapacity());
     if (this.currentCapacity() > this.loadFactor) {
       this.#expandBuckets();
     }
-    console.log(this.currentCapacity())
+    console.log(this.currentCapacity());
   }
-
-  // set = (key, value) => {
-  //   this.numberOfEntries++;
-  //   const index = this.hash(key);
-
-  //   if (index < 0 || index >= this.bucketsSize)
-  //     throw new Error("The index is out of bound");
-
-  //   if (this.buckets[index]) {
-  //     this.buckets[index].push([key, value]);
-  //     for (let i = 0; i < this.buckets[index].length; i++) {
-  //       if (this.buckets[index][0][0] === key) {
-  //         this.buckets[index][0][1] = value;
-  //       }
-  //     }
-  //   } else {
-  //     this.buckets[index] = [[key, value]];
-  //   }
-
-  //   console.log(this.numberOfEntries / this.bucketsSize);
-
-  //   if (this.numberOfEntries / this.bucketsSize > 0.75) {
-  //     this.resize();
-     
-  //   }
-  //   console.log(this.numberOfEntries / this.bucketsSize);
-  // };
-
-
-
-
-  
-
-
-
-
 
 
   get(key) {
-    const hashCode = this.#findAvailableCodeForKey(key);
-    if (hashCode === -1 || this.#_buckets[hashCode] === undefined) {
+    const hashCode = this.#findUnusedCodeForKey(key);
+    if (hashCode === -1 || this.#buckets[hashCode] === undefined) {
       return null;
     }
-    return this.#_buckets[hashCode];
+    return this.#buckets[hashCode];
   }
-
-  // get = (key) => {
-  //   const index = this.hash(key);
-
-  //   if (!this.#_buckets[index]) return null;
-
-  //   // return this.buckets[index].find((x) => x[0] === key)[1];
-  //   return this.#_buckets[index];
-  // };
 }
 
+const newHash = new HashMap();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const newHash = new HashMap(defaultConfig);
-
-console.log(newHash)
 newHash.set("apple", "red");
 newHash.set("banana", "yellow");
 newHash.set("carrot", "orange");
